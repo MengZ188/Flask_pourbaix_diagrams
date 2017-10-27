7f1aff2eb7303973bceecc435ceb876dd1f022cf
MengZ188 <32496960+MengZ188@users.noreply.github.com>
2017-10-27 10:30:16 -0700

    Update poubaix_plot.py

--- poubaix_plot.py
+++ poubaix_plot.py
@@ -786,19 +786,19 @@ _aqueous = """\
 
 
 
-refs = solvated('Zn')   
-refs1 = solvated('Cu')
+# refs = solvated('Zn')   
+# refs1 = solvated('Cu')
+# # print(refs)
+# # print(refs1)
+# # create a list contains all alloys or solids
+# refs += [('Zn', 0.0), ('ZnO', -3.323), ('ZnO2(aq)', -2.921), ('Cu',0.0)] 
+# #add solid and aqueous neutral species
+# refs += refs1 ##can we get reference energies from somewhere else?
 # print(refs)
-# print(refs1)
-# create a list contains all alloys or solids
-refs += [('Zn', 0.0), ('ZnO', -3.323), ('ZnO2(aq)', -2.921), ('Cu',0.0)] 
-#add solid and aqueous neutral species
-refs += refs1 ##can we get reference energies from somewhere else?
-print(refs)
-pb = Pourbaix(refs, Zn=1, Cu = 1, O =1) ##if it can be adjusted as elem0= 1, elem1 = 1, O = 1??
-U = np.linspace(-2, 2, 200)
-pH = np.linspace(-2, 16, 300)
-d, names, text = pb.diagram(U, pH, plot=True)
-#print(names)                                    ##ASE considers single metal system which 
-                                                #may be oversimplified in binary systems
-#print(type(pb.diagram(U, pH, plot=True)))
+# pb = Pourbaix(refs, Zn=1, Cu = 1, O =1) ##if it can be adjusted as elem0= 1, elem1 = 1, O = 1??
+# U = np.linspace(-2, 2, 200)
+# pH = np.linspace(-2, 16, 300)
+# d, names, text = pb.diagram(U, pH, plot=True)
+# #print(names)                                    ##ASE considers single metal system which 
+#                                                 #may be oversimplified in binary systems
+# #print(type(pb.diagram(U, pH, plot=True)))
