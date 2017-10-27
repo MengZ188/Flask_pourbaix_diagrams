32aa0844c6b6df8148dd8f0c6add9a8d70147918
MengZ188 <32496960+MengZ188@users.noreply.github.com>
2017-10-27 10:36:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ##flask app for pourbaix diagrams generation##
 
 ##first to do a very basic tutorial##
+import numpy as np
 import poubaix_plot
 
 from flask import Flask
@@ -11,16 +12,16 @@ def main():
     return "Welcome!"
 def pourbaix_generation():
     
-    refs = solvated('Zn')   
-    refs1 = solvated('Cu')
+    refs = pourbaix_plot.solvated('Zn')   
+    refs1 = pourbaix_plot.solvated('Cu')
     # print(refs)
     # print(refs1)
     # create a list contains all alloys or solids
     refs += [('Zn', 0.0), ('ZnO', -3.323), ('ZnO2(aq)', -2.921), ('Cu',0.0)] 
     #add solid and aqueous neutral species
     refs += refs1 ##can we get reference energies from somewhere else?
-    print(refs)
-    pb = Pourbaix(refs, Zn=1, Cu = 1, O =1) ##if it can be adjusted as elem0= 1, elem1 = 1, O = 1??
+    #print(refs)
+    pb = pourbaix_plot.Pourbaix(refs, Zn=1, Cu = 1, O =1) ##if it can be adjusted as elem0= 1, elem1 = 1, O = 1??
     U = np.linspace(-2, 2, 200)
     pH = np.linspace(-2, 16, 300)
     d, names, text = pb.diagram(U, pH, plot=True)
