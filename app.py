31c51dc2472a2e23be8c4cdd051fb136ff616bb0
MengZ188 <32496960+MengZ188@users.noreply.github.com>
2017-11-01 11:34:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,24 +16,21 @@ def pourbaix_generation():
     
     refs = solvated('Zn')   
     refs1 = solvated('Cu')
-    # print(refs)
-    # print(refs1)
-    # create a list contains all alloys or solids
     refs += [('Zn', 0.0), ('ZnO', -3.323), ('ZnO2(aq)', -2.921), ('Cu',0.0)] 
-    #add solid and aqueous neutral species
-    refs += refs1 ##can we get reference energies from somewhere else?
-    #print(refs)
-    pb = Pourbaix(refs, Zn=1, Cu = 1, O =1) ##if it can be adjusted as elem0= 1, elem1 = 1, O = 1??
+    refs += refs1 
+
+    pb = Pourbaix(refs, Zn=1, Cu = 1, O =1) 
     U = np.linspace(-2, 2, 200)
     pH = np.linspace(-2, 16, 300)
-#     d, names, text = 
-    pb.diagram(U, pH, plot=True)
-    import matplotlib.pyplot as plt
-    fig_1 = plt.gcf()
-#     import mpld3
-#     mpld3.save_json(fig_1,"/Users/mengzhao/Desktop/fig1_json")
+#   d, names, text = 
+    d, names, text= pb.diagram(U, pH, plot=True)
+    
+#     import matplotlib.pyplot as plt
+#     fig_1 = plt.gcf()
+# #     import mpld3
+# #     mpld3.save_json(fig_1,"/Users/mengzhao/Desktop/fig1_json")
    
-    return fig_1
+    return d, names, text
 
 if __name__== "__main__":
     app.run()
